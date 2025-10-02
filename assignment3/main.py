a98c83f9bd7c54d7d1bb268f3e6ecf33b0faeaee
rajandhital <38126008+rajandhital@users.noreply.github.com>
2025-10-02 21:48:22 +1000

    Update main.py
    review and update code

--- assignment3/main.py
+++ assignment3/main.py
@@ -6,8 +6,8 @@ This just launches the Tkinter GUI. Keep this file small and clean
 from assignment3.gui import App  # imports the App class from gui.py
 
 def main():
-    app = App()
-    app.mainloop()
+    app = App() # Initialize the GUI application
+    app.mainloop() # Run the Tkinter main loop (blocks until window closed)
 
 if __name__ == "__main__":
     main()
